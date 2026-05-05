8e1a505d703869d5fa967d6bbc6d0c6c0dcf5b81
ahmedelhappy <ahmedelhappy@gmail.com>
2026-05-06 00:07:46 +0300

    add i18n infrastructure: i18next config, translation files, RTL in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
 import { ThemeProvider } from './context/ThemeContext';
@@ -43,6 +45,13 @@ const PublicRoute = ({ children }) => {
 };
 
 function App() {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.lang = i18n.language;
+    document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
+  }, [i18n.language]);
+
   return (
     <ThemeProvider>
     <AuthProvider>
